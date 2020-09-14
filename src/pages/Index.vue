70542922ed877057b73e2d35bf5e65bc20b6c781
Alexander Bullock <alexanderbullock87@gmail.com>
2020-09-13 23:14:57 -0400

    fix index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,8 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="logo"
-      src="~assets/logo.png"
-    >
+  <q-page class='window-height window-width row justify-center items-center'>
+    <q-card style='width:80vw;min-width:400px;max-width:1200px' flat>
+      <img alt='logo' src='~assets/logo.png' />
+    </q-card>
   </q-page>
 </template>
 
